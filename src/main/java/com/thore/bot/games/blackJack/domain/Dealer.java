03339f5a688c40a1d986e20d73ab56bbebf3f537
Thore <mail@thoreeichhorn.de>
2023-03-05 13:47:54 +0100

    Vererbung von Person realisiert

--- src/main/java/com/thore/bot/games/blackJack/domain/Dealer.java
+++ src/main/java/com/thore/bot/games/blackJack/domain/Dealer.java
@@ -2,13 +2,13 @@ package com.thore.bot.games.blackJack.domain;
 
 public class Dealer extends Person {
 
-    public Dealer(String username, Hand hand) {
-        super(username, hand);
+    public Dealer() {
+        super("Dealer");
     }
 
-    public void printHandOfCards(){
-        System.out.println("The dealer has these cards");
-        System.out.println(super.getHand().getCard(0));
-        System.out.println("The second card is face down.");
-    }
+//    public void printHandOfCards(){
+//        System.out.println("The dealer has these cards");
+//        System.out.println(super.getHand().getCard(0));
+//        System.out.println("The second card is face down.");
+//    }
 }
