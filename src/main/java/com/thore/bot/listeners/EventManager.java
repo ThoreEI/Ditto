5ffbf206a9a23aca5f91d1664a692bd078d0d94c
Thore <mail@thoreeichhorn.de>
2023-03-09 23:35:52 +0100

    Methoden ausgelagert in spezifischere Listener-Klasse

--- src/main/java/com/thore/bot/listeners/EventManager.java
+++ src/main/java/com/thore/bot/listeners/EventManager.java
@@ -1,56 +1,24 @@
 package com.thore.bot.listeners;
-
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
-import java.util.Objects;
-
-
 public class EventManager implements EventListener {
-    private final static String BOT_SPAM_ID = "1065096620636643410";
-
     @Override
     public void onEvent(@NotNull GenericEvent event) {
-        if (event instanceof GuildMemberJoinEvent)
-            onGuildMemberJoin((GuildMemberJoinEvent) event);
-        if (event instanceof MessageReceivedEvent)
-            onMessageReceived((MessageReceivedEvent) event);
-        if (event instanceof MessageReactionAddEvent)
-            onMessageReactionAdd((MessageReactionAddEvent) event);
+        if (event instanceof GuildMemberJoinEvent) {
+            new StateListener().onGuildMemberJoin((GuildMemberJoinEvent) event);
+        }
+        if (event instanceof MessageReceivedEvent) {
+            new MessageListener().onMessageReceived((MessageReceivedEvent) event);
+        }
+        if (event instanceof MessageReactionAddEvent) {
+            new ReactionListener().onMessageReactionAdd((MessageReactionAddEvent) event);
+        }
     }
-
-    private static void onGuildMemberJoin(GuildMemberJoinEvent event) {
-        MessageChannel channel =
-                Objects.requireNonNull(
-                        Objects.requireNonNull(event.getGuild().getDefaultChannel())
-                                .asTextChannel());
-        String newMember = Objects.requireNonNull(event.getUser()).getAsTag();
-        channel.sendMessage("Welcome to the server " + newMember).queue();
-    }
-
-    private static void onMessageReactionAdd(MessageReactionAddEvent event) {
-        String user = Objects.requireNonNull(event.getUser()).getName();
-        String emoji = event.getReaction().getEmoji().getAsReactionCode();
-        MessageChannel channel = event.getChannel();
-        String message = "Reaction from " + user + ": " + emoji;
-        channel.sendMessage(message).queue();
-    }
-
-    private static void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot() || event.getChannel().getId().equals(BOT_SPAM_ID))  // TODO
-            return;
-        MessageChannel channel = event.getChannel();
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-        // TODO
-    }
+}
 //                    AceDiamonds.png"
 //                    AceHearts.png"
 //                    AceSpades.png"
@@ -104,6 +72,3 @@ public class EventManager implements EventListener {
 //                    TwoHearts.png"
 //                    TwoSpades.png"
 //                    AceClubs.png"
-
-
-}
