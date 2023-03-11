9fd81a98440b14b21fd61c3b97e8cfb0b17d20e1
Thore <mail@thoreeichhorn.de>
2023-03-11 20:46:26 +0100

    imports entfernt

--- src/main/java/com/thore/bot/listeners/ReactionListener.java
+++ src/main/java/com/thore/bot/listeners/ReactionListener.java
@@ -1,16 +1,12 @@
 package com.thore.bot.listeners;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Objects;
+
 import static com.thore.bot.listeners.EventManager.isBlackJackChannel;
 
 public class ReactionListener extends ListenerAdapter {
 
-    @Override
-    public void onButtonInteraction(ButtonInteractionEvent event) {
-        if (event.getUser().isBot() || !isBlackJackChannel(event.getChannel()))
-            return;
-        String username = event.getUser().getName();
-        String amountChips = event.getButton().getLabel();
-        event.getChannel().sendMessage(username + " setzt " + amountChips).queue();
-    }
+
 }
