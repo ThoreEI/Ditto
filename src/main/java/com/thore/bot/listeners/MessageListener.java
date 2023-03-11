10dd905eada5bcb61c9837e40537fde0d2b3480b
Thore <mail@thoreeichhorn.de>
2023-03-11 20:47:18 +0100

    redundanten Code entfernt

--- src/main/java/com/thore/bot/listeners/MessageListener.java
+++ src/main/java/com/thore/bot/listeners/MessageListener.java
@@ -8,22 +8,5 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import java.nio.channels.Channel;
 
 public class MessageListener extends ListenerAdapter {
-//    @Override
-//    public void onMessageReceived(MessageReceivedEvent event) {
-//        if (isBlackJackChannel(event.getChannel()) || isBotSpamChannel(event.getChannel())
-//                || event.getAuthor().isBot()
-//                || event.isWebhookMessage())
-//            return;
-//        String content = event.getMessage().getContentRaw();
-//        BlackJackGame.BLACK_JACK_GAME_CHANNEL.sendMessage("\""+ content + "\"").queue();
-//    }
 
-
-    private boolean isBlackJackChannel(MessageChannel channel) {
-        return channel.getId().equals(Bot.getConfig().get("BLACK_JACK_GAME_ID"));
-    }
-
-    private boolean isBotSpamChannel(MessageChannel channel) {
-        return channel.getId().equals(Bot.getConfig().get("BOT_SPAM_ID"));
-    }
 }
