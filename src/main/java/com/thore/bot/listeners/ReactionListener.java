55a2f86a9575820d268e0c6cbfcdf28214afc41f
Thore <mail@thoreeichhorn.de>
2023-03-11 03:46:28 +0100

    refactored

--- src/main/java/com/thore/bot/listeners/ReactionListener.java
+++ src/main/java/com/thore/bot/listeners/ReactionListener.java
@@ -5,17 +5,18 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import static com.thore.bot.listeners.EventManager.isBlackJackChannel;
+import static com.thore.bot.listeners.EventManager.isBotSpamChannel;
 
 public class ReactionListener extends ListenerAdapter {
 
-    @Override
-    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-    super.onMessageReactionAdd(event);
-    }
+//    @Override
+//    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
+//        super.onMessageReactionAdd(event);
+//    }
 
     @Override
     public void onButtonInteraction(ButtonInteractionEvent event) {
-        if (!isBlackJackChannel(event.getChannel()))
+        if (event.getUser().isBot() || !isBlackJackChannel(event.getChannel()))
             return;
         String username = event.getUser().getName();
         String amountChips = event.getButton().getLabel();
