c961a3b56c89aff5145d5f2ea7593d0c7d8880ec
Thore <mail@thoreeichhorn.de>
2023-03-13 00:19:52 +0100

    um Abfrage erweitert

--- src/main/java/com/thore/bot/listeners/ButtonListener.java
+++ src/main/java/com/thore/bot/listeners/ButtonListener.java
@@ -2,6 +2,7 @@ package com.thore.bot.listeners;
 
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.util.Objects;
 
@@ -11,10 +12,12 @@ public class ButtonListener extends ListenerAdapter {
 
     @Override
     public void onButtonInteraction(ButtonInteractionEvent event) {
-        if (event.getUser().isBot() || !isBlackJackChannel(event.getChannel()))
+        if (event.getUser().isBot()||!isBlackJackChannel(event.getChannel()))
             return;
-        String amountChips = event.getButton().getLabel();
-        if (Objects.equals(event.getButton().getId(), "btnChip1") /* TODO  player chips */)
-            event.getChannel().sendMessage(event.getUser().getName() + " setzt " + amountChips).queue();
+        int amountChips = Integer.parseInt(event.getButton().getLabel());
+       // if (amountChips==1||amountChips==5||amountChips==10||amountChips==25||amountChips==50 ||amountChips==100||amountChips==200||amountChips==500||amountChips==1000||amountChips==5000)
+            if (Objects.equals(event.getButton().getId(), "btnChip" + amountChips))
+                event.getUser().getName();
+        event.getChannel().sendMessage(event.getUser().getName() + " setzt " + amountChips).queue();
     }
 }
