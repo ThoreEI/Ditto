d01b925275bea94050b8b2725b6aa94f74f5f85c
Thore <mail@thoreeichhorn.de>
2023-03-13 00:20:14 +0100

    refactored

--- src/main/java/com/thore/bot/io/reader/FileReader.java
+++ src/main/java/com/thore/bot/io/reader/FileReader.java
@@ -8,18 +8,21 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class FileReader {
+public class FileReader  {
     private final static String JOKES_TXT = "src/main/java/com/thore/bot/io/files/txtFiles/jokes.txt";
     private final static String CARDS_PNG = "src/main/java/com/thore/bot/io/files/pngFiles/";
 
     public static String loadJoke() {
         String readLines = "";
+
         try {
             readLines = Files.lines(Paths.get(JOKES_TXT)).collect(Collectors.joining(System.lineSeparator()));
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            System.err.println("An error occurred while loading a joke.");
         }
+
         String[] jokes = readLines.split("\n");
         return jokes[new Random().nextInt(jokes.length)];
     }
