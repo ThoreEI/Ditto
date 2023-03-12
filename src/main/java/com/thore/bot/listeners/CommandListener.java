98041f284d42cb2a650ebd06f99fdbc61af6da62
Thore <mail@thoreeichhorn.de>
2023-03-12 02:32:02 +0100

    Reaktion auf /blackJack erweitert

--- src/main/java/com/thore/bot/listeners/CommandListener.java
+++ src/main/java/com/thore/bot/listeners/CommandListener.java
@@ -2,20 +2,32 @@ package com.thore.bot.listeners;
 import com.thore.bot.Bot;
 import com.thore.bot.games.blackJack.blackJackGame.BlackJackGame;
 import com.thore.bot.io.reader.FileReader;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class CommandListener extends ListenerAdapter {
     public final static String BOT_SPAM_CHANNEL = Bot.getConfig().get("BOT_SPAM_ID");
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-      if (BOT_SPAM_CHANNEL.equals(event.getChannel().getId()))
+        if (BOT_SPAM_CHANNEL.equals(event.getChannel().getId()))
             return;
-        if (event.getName().equals("joke")) {
+        else if (event.getName().equals("joke"))
             event.reply(FileReader.loadJoke()).queue();
-        } else if (event.getName().equals("blackjack")) {
+        else if (event.getName().equals("blackjack")) {
+            Category blackjack = Objects.requireNonNull(
+                            event.getGuild())
+                    .getCategoriesByName("blackjack", true)
+                    .get(0);
+            TextChannel temporaryChannel = blackjack.createTextChannel("Black Jack Game " + blackjack.getChannels().size())
+                    .setTopic("This is a temporary Black Jack channel.")
+                    .complete();
             event.reply("Blackjack is starting...").queue();
             new BlackJackGame();
         }
