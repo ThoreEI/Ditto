2046a3389c7287457f43074199609160818da64e
Thore <mail@thoreeichhorn.de>
2023-03-12 02:44:08 +0100

    Abfrage nach richtigem Channel ergänzt

--- src/main/java/com/thore/bot/listeners/CommandListener.java
+++ src/main/java/com/thore/bot/listeners/CommandListener.java
@@ -2,7 +2,6 @@ package com.thore.bot.listeners;
 import com.thore.bot.Bot;
 import com.thore.bot.games.blackJack.blackJackGame.BlackJackGame;
 import com.thore.bot.io.reader.FileReader;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -11,16 +10,17 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+import static com.thore.bot.listeners.EventManager.*;
+
 public class CommandListener extends ListenerAdapter {
-    public final static String BOT_SPAM_CHANNEL = Bot.getConfig().get("BOT_SPAM_ID");
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        if (BOT_SPAM_CHANNEL.equals(event.getChannel().getId()))
+        if (isBotSpamChannel(event.getChannel()))
             return;
-        else if (event.getName().equals("joke"))
+        else if (event.getName().equals("joke") && isJokesChannel(event.getChannel()))
             event.reply(FileReader.loadJoke()).queue();
-        else if (event.getName().equals("blackjack")) {
+        else if (event.getName().equals("blackjack") && isBlackJackChannel(event.getChannel())) {
             Category blackjack = Objects.requireNonNull(
                             event.getGuild())
                     .getCategoriesByName("blackjack", true)
@@ -29,7 +29,7 @@ public class CommandListener extends ListenerAdapter {
                     .setTopic("This is a temporary Black Jack channel.")
                     .complete();
             event.reply("Blackjack is starting...").queue();
-            new BlackJackGame();
+            new BlackJackGame(temporaryChannel);
         }
 
         // Erinnerung --> bump --> 100 Coins --> BlackJack --> Bestenliste
