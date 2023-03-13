93d72e5aea1a67c1b0b50cfd49f31359d70dec81
Thore <mail@thoreeichhorn.de>
2023-03-13 15:25:56 +0100

    ID Handler als Klasse implementiert

--- src/main/java/com/thore/bot/IDHandler.java
+++ src/main/java/com/thore/bot/IDHandler.java
@@ -1,11 +1,12 @@
-package com.thore.bot.channel;
+package com.thore.bot;
 import com.thore.bot.Bot;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
-public class ChannelManager {
+public class IDHandler {
     private final static TextChannel BLACK_JACK_GAME_CHANNEL = Bot.getJda().getTextChannelById(Bot.getConfig().get("BLACK_JACK_GAME_ID"));
     private final static TextChannel BOT_SPAM_CHANNEL = Bot.getJda().getTextChannelById(Bot.getConfig().get("BOT_SPAM_ID"));
     private final static TextChannel JOKES_CHANNEL = Bot.getJda().getTextChannelById(Bot.getConfig().get("JOKES_ID"));
+
     public static TextChannel getJokesChannel() {
         return JOKES_CHANNEL;
     }
